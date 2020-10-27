0946cfb95ab1cb18c3aed6190c9da31dd4a06244
Philippe Brossier <brossierp@gmail.com>
2020-10-27 21:51:55 +0000

    1st commit. WIP

---
+++ src/main/java/com/example/akspoc/AkspocApplication.java
@@ -0,0 +1,13 @@
+package com.example.akspoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkspocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkspocApplication.class, args);
+	}
+
+}
